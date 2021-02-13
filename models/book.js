35101d224b968a8d26cd3f645ea0b2413124520f
apemint <escobar.andres.alfonso@gmail.com>
2021-02-13 14:06:13 -0500

    ahhh

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //Schema
-const bookSchema = new Schema({
+const BookSchema = new Schema({
   authors: {},
   description: String,
   image: {},
@@ -10,6 +10,6 @@ const bookSchema = new Schema({
   title: { type: String, required: true },
 });
 
-const Book = mongoose.model("book", bookSchema);
+const Book = mongoose.model("Book", BookSchema);
 
 module.exports = Book;
